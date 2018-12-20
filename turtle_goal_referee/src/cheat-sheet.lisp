b7c4cc91d558160ce768d0ebda633f8b8a0a66dd
Gayane Kazhoyan <gayanek@gmail.com>
2018-12-20 16:17:41 +0100

    made cheat-sheet compile

--- turtle_goal_referee/src/cheat-sheet.lisp
+++ turtle_goal_referee/src/cheat-sheet.lisp
@@ -16,40 +16,44 @@
 (defvar *turtle-pose* (cpl:make-fluent :name :turtle-pose) "current pose of turtle")
 (defparameter *pose-sub* nil) ;; pose subscriber
 
+(defun pose-cb (msg)
+  "Callback for pose values. Called by the pose topic subscriber."
+  (setf (cpl:value *turtle-pose*) msg))
+
 (defun init ()
   (setf *pose-sub*
         (roslisp:subscribe "turtle1/pose"
                            "turtlesim/Pose"
                            #'pose-cb)))
+; (roslisp:start-ros-node "lisp_node")
+(init)
 ;; Each message on the pose topic executes 'pose-cb with the message as parameter
-(defun pose-cb (msg)
-  "Callback for pose values. Called by the pose topic subscriber."
-  (setf (cpl:value *turtle-pose*) msg))
 ;; *turtle-pose* is now a fluent variable after calling the init function
 
 ;; Use fluent values with 'fl-funcall and 'value
 
 ;; get values
-(value *turtle-pose*)
+(cpl:value *turtle-pose*)
 
 ;; call function on fluent
 ;; e.g. get x-value from the pose
-(fl-funcall #'turtlesim-msg:x *turtle-pose*)
+(cpl:fl-funcall #'turtlesim-msg:x *turtle-pose*)
 ;; turtlesim-msg:x is the getter for the x value
 ;; on a normal Pose message you would just call (turtlesim-msg:x pose)
 ;; here you apply the getter on the fluent object, to get the x as another fluent 
 
 ;; react to the values in a fluent with 'wait-for
-(wait-for (< (fl-funcall #'turtlesim-msg:x *turtle-pose*)
-             5.0))
+(cpl:wait-for (cpl:< (cpl:fl-funcall #'turtlesim-msg:x *turtle-pose*)
+                     5.0))
 ;; blocks the current process until the turtle's x is smaller that 5
 
 ;; Create a command publisher for the turtle
 (defvar *cmd-vel-pub* nil "velocity commands ROS publisher")
 (defun init-cmd-pub ()
   (init)
-  (setf *cmd-vel-pub* (advertise "turtle1/cmd_vel"
-                                 "geometry_msgs/Twist")))
+  (setf *cmd-vel-pub* (roslisp:advertise "turtle1/cmd_vel"
+                                         "geometry_msgs/Twist")))
+(init-cmd-pub)
 ;; Execute this after some idle time, when the publisher expired.
 ;; We use this function to move the turtle
 (defun send-vel-cmd (lin ang)
@@ -67,13 +71,14 @@
   (loop do
     (send-vel-cmd 3 (/ pi 5))
     (wait-duration 0.1)))
+;; To stop the endless loop press Ctrl-C Ctrl-C
 
 ;; Connect it with the fluent condition and execute the movement and condition as a plan
 ;; This will not be executable out of the box, it's only an example
 ;; (cpl:pursue
 ;;   (wait-for (> (fl-funcall #'turtlesim-msg:x *turtle-pose*)
 ;;                5.0))
-;;   (loop do 
+;;   (loop do
 ;;     (send-vel-cmd 1 (/ pi 10))
 ;;     (wait-duration 0.1)))
 
@@ -82,8 +87,8 @@
 ;; You need to wrap the call above in a top-level-plan to make it work:
 (cpl:top-level
   (cpl:pursue
-    (cpl:wait-for (> (cpl:fl-funcall #'turtlesim-msg:x *turtle-pose*)
-                     5.0))
+    (cpl:wait-for (cpl:> (cpl:fl-funcall #'turtlesim-msg:x *turtle-pose*)
+                         5.0))
     (draw-circle)))
 ;; Pursue will terminate, as soon as one of the functions terminate
 ;; Here it terminates when x is greater than 5,
@@ -92,33 +97,39 @@
 ;; Include a 'finally' statement, which executes a code block after the plan,
 ;; with 'unwind-protect
 (cpl:top-level
-  (unwind-protect
-       (cpl:pursue
-         (wait-for (> (fl-funcall #'turtlesim-msg:x *turtle-pose*)
-                      5.0))
-         (draw-circle))
-    (send-vel-cmd 0 0)))
+  (cpl:pursue
+    (unwind-protect
+         (cpl:wait-for (cpl:>
+                        (cpl:fl-funcall #'turtlesim-msg:x
+                                        *turtle-pose*)
+                        5.0))
+      (send-vel-cmd 0 0))
+    (draw-circle)))
 ;; The second body of unwind-protect:
 ;; (send-vel-cmd 0 0)
-;; will be executed immediately, when pursue terminates,
+;; will be executed immediately, when wait-for terminates,
 ;; preventing the turtle to move after the condition is met (x > 5).
 
 
 ;; You can catch a condition and execute something, to recover from an error
 ;; Lets quickly define a condition
 (define-condition out-of-bounds-error (cpl:simple-plan-failure) ())
+;; It has to be a child of CPL:SIMPLE-PLAN-FAILURE
 
 ;; And a function that checks the condition for fluents
-(defun out-of-bounds (fluent-pose)
-  (roslisp:with-fields (x y) (cpl:value fluent-pose)
-    (not (and (< 0.5 x 10.5)
-              (< 0.5 y 10.5)))))
+(defun out-of-bounds (fluent-position)
+  (roslisp:with-fields (turtlesim-msg:x turtlesim-msg:y)
+      (cpl:value fluent-position)
+    (not (and (< 0.5 turtlesim-msg:x 10.5)
+              (< 0.5 turtlesim-msg:y 10.5)))))
 
 ;; With the following we will throw an error without handling it
+;; seq is alias for progn
 (cpl:top-level
   (cpl:pursue
-    (cpl:whenever ((cpl:fl-funcall #'out-of-bounds *turtle-pose*))
-      (cpl:fail 'out-of-bounds-error)) ;; throws the error
+    (cpl:seq
+      (cpl:wait-for (cpl:fl-funcall #'out-of-bounds *turtle-pose*))
+      (cpl:fail 'out-of-bounds-error))   ; throws the error
     (draw-circle)))
 
 ;; Lets handle the error and try to recover
@@ -128,23 +139,24 @@
        (declare (ignore e))
        (roslisp:ros-warn (ooberror) "Out of bounds!")
        (send-vel-cmd -1 0)
-       (sleep 1)
+       (cpl:sleep 1)
        (cpl:retry)))
    (cpl:pursue
-    (cpl:whenever ((cpl:fl-funcall #'out-of-bounds *turtle-pose*))
-                  (cpl:fail 'out-of-bounds-error)) ;; throws the error
+    (cpl:seq
+      (cpl:wait-for (cpl:fl-funcall #'out-of-bounds *turtle-pose*))
+      (cpl:fail 'out-of-bounds-error))   ; throws the error
     (draw-circle))))
 
-;; another example for whenever
+;; example of whenever
 (cpl:par
     (cpl:whenever (*fluent*)
       (print "oh fluent is true")
       (setf (cpl:value *fluent*) NIL))
-    (loop for i from 1 to 1000000 do 
+    (loop for i from 1 to 1000000 do
       (cpl:sleep 1.0)
       (setf (cpl:value *fluent*) T)))
 
-;; execute plans in order with try-in-order. seq is like progn
+;; execute plans in order with try-in-order.
 (cpl:top-level
   (cpl:try-in-order
     (cpl:seq
